6e4244f83cfd9cdf33ad34666e632245f3278fcf
BakariL <bakarilewis@gmail.com>
2018-03-13 08:42:40 -0400

    first

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReviewAppFinal.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Display(Name = "Category")]
+        public string Name { get; set; }
+
+        //Navigation property
+        public virtual ICollection<Review> Review
+        { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReviewAppFinal.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        [Display(Name = "Release Date: ")]
+        public DateTime RelaseDate { get; set; }
+        [Display(Name = "Description: ")]
+        public string Description { get; set; }
+        [Display(Name = "Tech Crunch Review: ")]
+        public int ReviewScore { get; set; }
+
+        [ForeignKey("Category")]
+        public int CateogryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
